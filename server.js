1c5c9cc769b83889082d73fd5e93744b1f95274e
Your Name <fabien.lartin@docdoku.com>
2021-05-17 10:39:20 +0200

    Working

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 }); */
 
-require("./app/routes/joueurs.routes.js")(app);
+require("./routes/joueurs.routes.js")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
